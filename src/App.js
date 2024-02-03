ea11bcca31b25619bbe5e663064061c4fcfe68f1
Anmol Verma <anmol.verma4@gmail.com>
2024-02-03 15:10:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import VideoPlayer from './video';
+import './App.css'
 
-function App() {
+import '@theonlyducks/videojs-zoom/styles';
+import '@theonlyducks/videojs-zoom';
+
+const App = () => {
+  const videoUrl = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = {
+    controls: true, // Show video controls
+        controlBar: {
+          fullscreenToggle: true // Enable the fullscreen button in the control bar
+        },
+        playbackRates: [0.5, 1, 1.5, 2, 4],
+    autoplay: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src:  videoUrl,
+      type: 'video/mp4'
+    }]
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+    player.zoomPlugin({
+      showZoom: true,
+      showMove: true,
+      showRotate: true,
+      gestureHandler: false
+    });
+    player.seekButtons({
+      forward: 30,
+      back: 10
+    });
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <VideoPlayer options={videoJsOptions} onReady={handlePlayerReady} />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/video.js
@@ -0,0 +1,46 @@
+import React, { useRef, useEffect } from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css'; // Import Video.js CSS
+import '@antmedia/videojs-webrtc-plugin';
+import 'videojs-playbackrate-adjuster';
+import 'videojs-seek-buttons';
+import '@theonlyducks/videojs-zoom';
+import '@theonlyducks/videojs-zoom/styles';
+import 'videojs-seek-buttons/dist/videojs-seek-buttons.css'
+
+
+const VideoPlayer = (props) => {
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
+  const {options, onReady} = props;
+
+  useEffect(() => {
+    // Make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode. 
+      const videoElement = document.createElement("video-js");
+
+      videoElement.classList.add('vjs-big-play-centered');
+      videoRef.current.appendChild(videoElement);
+
+      const player = playerRef.current = videojs(videoElement, options, () => {
+        onReady && onReady(player);
+      });
+
+    } else {
+      const player = playerRef.current;
+     
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
+      
+    }
+  }, [options, videoRef]);
+
+  return (
+    <div data-vjs-player>
+      <div ref={videoRef} />
+    </div>
+  );
+}
+
+export default VideoPlayer;
